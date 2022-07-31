4a3c92e3c60fd00b9d38c236e3f3dafa4b2c19f8
iqbalhosen32 <iqbaldb32@gmail.com>
2022-07-31 14:16:06 +0600

    Project Complete

--- client/src/Components/PostDetails/PostDetails.js
+++ client/src/Components/PostDetails/PostDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Paper, Typography, CircularProgress, Divider, Card, Grid } from '@material-ui/core';
+import { Paper, Typography, CircularProgress, Divider, Grid } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 import { useParams, useNavigate } from 'react-router-dom';
